d1d6f207231d15adce2587b57fc95376eb227533
Takashi Kokubun <takashikkbn@gmail.com>
2019-05-11 20:37:51 +0900

    Roughly disassemble constant pool

--- src/main/java/com/github/k0kubun/jjvm/bytecode/ClassFile.java
+++ src/main/java/com/github/k0kubun/jjvm/bytecode/ClassFile.java
@@ -19,6 +19,20 @@ public class ClassFile {
         builder.append(String.format("  magic: 0x%X\n", magic));
         builder.append(String.format("  minor version: %d\n", minorVersion));
         builder.append(String.format("  major version: %d\n", majorVersion));
+        builder.append(String.format("  flags: TODO\n"));
+        builder.append(disassembleConstantPool());
+        return builder.toString();
+    }
+
+    private String disassembleConstantPool() {
+        StringBuilder builder = new StringBuilder();
+        builder.append("Constant pool:\n");
+        for (int i = 0; i < constantPool.length; i++) {
+            String typeName = constantPool[i].getType().toString();
+            builder.append(String.format("%5s = %-19s", String.format("#%d", i + 1), typeName));
+            builder.append("TODO");
+            builder.append("\n");
+        }
         return builder.toString();
     }
 }
