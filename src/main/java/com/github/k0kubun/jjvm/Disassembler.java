e775defd09836770054d5aee198dd7ffa2ac1d94
Takashi Kokubun <takashikkbn@gmail.com>
2019-05-30 05:06:30 +0900

    Construct basic jjvm CLI

--- src/main/java/com/github/k0kubun/jjvm/Disassembler.java
+++ src/main/java/com/github/k0kubun/jjvm/Disassembler.java
@@ -4,21 +4,25 @@ import com.github.k0kubun.jjvm.classfile.ClassFile;
 import com.github.k0kubun.jjvm.classfile.ClassFileDisassembler;
 import com.github.k0kubun.jjvm.classfile.ClassFileParser;
 
-import java.io.FileNotFoundException;
 import java.io.IOException;
 
+// $ jjvmp [class file]
 public class Disassembler {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         if (args.length != 1) {
             System.out.println("Usage: jjvmp [class file]");
             return;
         }
 
+        ClassFile classFile;
         try {
-            ClassFile classFile = new ClassFileParser().parse(args[0]);
-            System.out.println(new ClassFileDisassembler(classFile).disassemble());
-        } catch (FileNotFoundException e) {
+            classFile = new ClassFileParser().parse(args[0]);
+        } catch (IOException e) {
             System.out.println("Failed to open: " + e.getMessage());
+            return;
         }
+
+        String disasm = new ClassFileDisassembler(classFile).disassemble();
+        System.out.println(disasm);
     }
 }
--- src/main/java/com/github/k0kubun/jjvm/JJVM.java
+++ src/main/java/com/github/k0kubun/jjvm/JJVM.java
@@ -1,7 +1,26 @@
 package com.github.k0kubun.jjvm;
 
+import com.github.k0kubun.jjvm.classfile.ClassFile;
+import com.github.k0kubun.jjvm.classfile.ClassFileParser;
+
+import java.io.IOException;
+
+// $ jjvm [class file]
 public class JJVM {
     public static void main(String[] args) {
+        if (args.length != 1) {
+            System.out.println("Usage: jjvm [class file]");
+            return;
+        }
+
+        ClassFile classFile;
+        try {
+            classFile = new ClassFileParser().parse(args[0]);
+        } catch (IOException e) {
+            System.out.println("Failed to open: " + e.getMessage());
+            return;
+        }
+
         System.out.println("Start JJVM!");
     }
 }
