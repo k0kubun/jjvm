5fc2cd92a00b1b772c1d871ddc7fe5b2cf9ca5ab
Takashi Kokubun <takashikkbn@gmail.com>
2019-06-16 18:53:40 +0900

    Avoid opening boot jar files many times

--- src/main/java/com/github/k0kubun/jjvm/virtualmachine/ClassLoader.java
+++ src/main/java/com/github/k0kubun/jjvm/virtualmachine/ClassLoader.java
@@ -8,17 +8,19 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
 
 public class ClassLoader {
-    private final List<String> bootPaths;
+    private final Map<String, String> jarByClass;
     private final List<String> classPaths;
 
     public ClassLoader(String classPath) {
-        bootPaths = new ArrayList<>();
-        setupBootstrapSearchPath(bootPaths);
+        jarByClass = new HashMap<>();
+        setupBootstrapSearchPath(jarByClass);
 
         classPaths = new ArrayList<>();
         for (String path : classPath.split(":")) {
@@ -29,19 +31,19 @@ public class ClassLoader {
     public ClassFile loadClass(String klass) {
         String filename = String.format("%s.class", klass);
 
-        for (String bootPath : bootPaths) {
-            try (ZipInputStream jarStream = new ZipInputStream(new FileInputStream(bootPath))) {
+        if (jarByClass.containsKey(filename)) {
+            String jarPath = jarByClass.get(filename);
+            try (ZipInputStream jarStream = new ZipInputStream(new FileInputStream(jarPath))) {
                 ZipEntry entry;
                 while ((entry = jarStream.getNextEntry()) != null) {
                     if (entry.getName().equals(filename)) {
                         return new ClassFileParser().parse(jarStream);
                     }
                 }
-            } catch (FileNotFoundException e) {
-                // expected for "jre/lib/sunrsasign.jar" and "jre/classes"
             } catch (IOException e) {
-                System.out.println(String.format("Error while loading '%s': %s", bootPath, e.toString()));
+                System.out.println(String.format("Error while loading '%s': %s", jarPath, e.toString()));
             }
+            throw new RuntimeException(String.format("Failed to load '%s' from '%s'", filename, jarPath));
         }
 
         for (String classPath : classPaths) {
@@ -58,10 +60,26 @@ public class ClassLoader {
         throw new RuntimeException(String.format("Class '%s' was not found in classpath", klass));
     }
 
-    private void setupBootstrapSearchPath(List<String> paths) {
+    private void setupBootstrapSearchPath(Map<String, String> jarByClass) {
         String bootclasspath = System.getProperty("sun.boot.class.path"); // java.class.path in Java 9+
-        for (String path : bootclasspath.split(":")) {
-            paths.add(path);
+
+        for (String jarPath : bootclasspath.split(":")) {
+            try (ZipInputStream jarStream = new ZipInputStream(new FileInputStream(jarPath))) {
+                ZipEntry entry;
+                while ((entry = jarStream.getNextEntry()) != null) {
+                    if (entry.getName().endsWith(".class")) {
+                        if (jarByClass.containsKey(entry.getName())) {
+                            throw new RuntimeException(String.format("'%s' exists in both '%s' and '%s'",
+                                    entry.getName(), jarByClass.get(entry.getName()), jarPath));
+                        }
+                        jarByClass.put(entry.getName(), jarPath);
+                    }
+                }
+            } catch (FileNotFoundException e) {
+                // expected for "jre/lib/sunrsasign.jar" and "jre/classes"
+            } catch (IOException e) {
+                System.out.println(String.format("Error while loading '%s': %s", jarPath, e.toString()));
+            }
         }
     }
 }
